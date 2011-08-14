db3cc8c9997e0bf59b0e962631c22cdfbfe34964
Rafael Biriba <rafaelbiriba@corp.globo.com>
2011-08-13 23:36:16 -0300

    adicionando toda a parte de login e logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,14 +13,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:success] = "Welcome to the sample app"
-      redirect_to user_path(@user)
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       @title = "Sign up"
       @user.password = ""
       render 'new'
     end
   end
-
 
 end
