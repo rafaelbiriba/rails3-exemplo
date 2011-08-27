e9b3eef7689930d52a4b7d6f316d26051b0d55e2
Rafael Biriba <rafaelbiriba@corp.globo.com>
2011-08-27 17:56:21 -0300

    added user microposts

---
+++ app/models/micropost.rb
@@ -0,0 +1,22 @@
+class Micropost < ActiveRecord::Base
+  belongs_to :user
+  attr_accessible :content
+
+  validates :content, :presence => true, :length => { :maximum => 140 }
+  validates :user_id, :presence => true
+
+  default_scope :order => 'microposts.created_at DESC'
+end
+
+# == Schema Information
+# Schema version: 20110827192940
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
-
 class User < ActiveRecord::Base
   attr_accessor :password
+  has_many :microposts, :dependent => :destroy
+
   attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex =/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -25,6 +26,11 @@ class User < ActiveRecord::Base
    def self.authenticate(email, submitted_password)
      user = find_by_email(email)
      user && user.has_password?(submitted_password) ? user : nil
+   end
+
+   def feed
+       # This is preliminary. See Chapter 12 for the full implementation.
+       Micropost.where("user_id = ?", id)
    end
 
 
